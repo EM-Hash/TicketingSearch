ece360ec57538a31f991e5ae0b5f8c90af701cd1
EM-Hash <emeade3@my.wctc.edu>
2021-09-29 17:30:58 -0500

    Added debug and setup Ticket class

--- Ticket.cs
+++ Ticket.cs
@@ -2,17 +2,37 @@ using System;
 
 namespace TicketingOOP{
     public class Ticket{
-    /* Each ticket has a:
-    - ID
-    - Summary
-    - Status
-    - Priority
-    - Submitter
-    - Assigner
-    - Watching
-    */
+        //Values w/ getters
+        int id {get;}
+        string summary {get;}
+        string status {get;}
+        string priority {get;}
+        string submitter {get;}
+        string assigner {get;}
+        string watching {get;}
 
-    //Have method to get the ticket
-    //Have two constructors; one for no entry,and one for an entry of all values
+        //Constructors 
+
+        //Constructor for no arguments passed in
+        public Ticket(){
+            //Do nothing, it's just made now
+        }
+
+        //Constructor for when all arguments are given
+        public Ticket(int id, string summary, string status, 
+        string priority, string submitter, string assigner, string watching){
+            this.id = id;
+            this.summary = summary;
+            this.status = status;
+            this.priority = priority;
+            this.submitter = submitter;
+            this.assigner = assigner;
+            this.watching = watching;
+        }
+
+        //Method for printing ticket
+        public string getTicket(){
+            return($"{id,-3} | {summary,-25} | {priority,-7} | {submitter,-15} | {assigner,-15} {watching, -30}");
+        }
     }
 }
